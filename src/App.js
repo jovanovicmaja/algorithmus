b6018f35c9d644056cb61f8fc2274479b3a7b9a4
Maja Jovanovic <maja.jovanovic@adnovum.ch>
2021-01-12 09:23:46 +0100

    Inserted random text

--- src/App.js
+++ src/App.js
@@ -31,7 +31,66 @@ const App = () => {
           </Form>
         </Navbar>
       </header>
-      <main style={{ margin: '20px 40px 0 40px' }}></main>
+      <main style={{ margin: '20px 60px 0 60px' }}>
+        <div>
+          <h1>Lorem Ipsum Text</h1>
+          <div>
+            <h2>Erster Abschnitt</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua. At vero eos et accusam et justo duo
+              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+              sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+              amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+              invidunt ut labore et dolore magna aliquyam erat, sed diam
+              voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+              Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+              dolor sit amet.
+            </p>
+          </div>
+          <div>
+            <h2>Zweiter Abschnitt</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua. At vero eos et accusam et justo duo
+              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+              sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure
+              dolor in hendrerit in vulputate velit esse molestie consequat, vel
+              illum dolore eu feugiat nulla facilisis at vero eros et accumsan
+              et iusto odio dignissim qui blandit praesent luptatum zzril
+              delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum
+              dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
+              nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+              volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+              ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+              consequat. Duis autem vel eum iriure dolor in hendrerit in
+              vulputate velit esse molestie consequat, vel illum dolore eu
+              feugiat nulla facilisis at vero eros et accumsan et iusto odio
+              dignissim qui blandit praesent luptatum zzril delenit augue duis
+              dolore te feugait nulla facilisi. Nam liber tempor cum soluta
+              nobis eleifend option congue nihil imperdiet doming id quod mazim
+              placerat facer possim assum.
+            </p>
+            <div>
+              <h3>Untertitel</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+                diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+                aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+                nostrud exerci tation ullamcorper suscipit lobortis nisl ut
+                aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor
+                in hendrerit in vulputate velit esse molestie consequat, vel
+                illum dolore eu feugiat nulla facilisis. At vero eos et accusam
+                et justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+                sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+                dolor sit amet, consetetur
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
